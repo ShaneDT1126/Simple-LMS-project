6bb373b5d1c56a4ce0697ecde0377a9dfb574954
Shijuchi <shanedelatorre2412@gmail.com>
2024-06-21 00:34:51 +0800

    static front page done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar" role="navigation" aria-label="main navigation" style="min-height: 5rem">
+    <div class="navbar-brand">
+      <a class="navbar-item is-size-4">StudHere</a>
+    </div>
+
+    <div id="navbar-item" class="navbar-menu">
+
+      <div class="navbar-start">
+        <a class="navbar-item">Home</a>
+        <a class="navbar-item">About</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Signup</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
   </nav>
+
   <router-view/>
+
+  <footer>
+    <div class="footer">
+      <p class="has-text-centered">Copyright (c) 2024</p>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,90 @@
 <template>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="hero is-info is-medium">
+      <div class="hero-body has-text-centered">
+        
+        <h1 class="title">
+          Welcome to StudHere
+        </h1>
+
+        <h2 class="subtitle">
+          An online place for learning.
+        </h2>
+
+      </div>
+    </div>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns is-multiline">
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-info">
+                <i class="far fa-clock"></i>
+              </span>
+
+              <h2 class="is-size-4 mt-4 mb-4">
+                  Study at your own pace
+              </h2>
+
+              <p>Gawhdem Gawhdem Gawhdem</p>
+              
+            </div>
+          </div>
+
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-info">
+                <i class="far fa-comments"></i>
+              </span>
+
+              <h2 class="is-size-4 mt-4 mb-4">
+                  Study with other people
+              </h2>
+
+              <p>Gawhdem Gawhdem Gawhdem</p>
+              
+            </div>
+          </div>
+
+          <div class="column is-4">
+            <div class="box has-text-centered">
+              <span class="icon is-size-2 has-text-info">
+                <i class="far fa-home"></i>
+              </span>
+
+              <h2 class="is-size-4 mt-4 mb-4">
+                  Study from everywhere
+              </h2>
+
+              <p>Gawhdem Gawhdem Gawhdem</p>
+              
+            </div>
+          </div>
+
+          <div class="column is-12 has-text-centered">
+            <a class="button is-info is-size-3 mt-6 mb-6">
+              Get Started
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
+
   components: {
-    HelloWorld
+    
   }
+  
 }
 </script>
