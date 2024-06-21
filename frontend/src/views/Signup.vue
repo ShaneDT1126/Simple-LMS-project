240918a2e1db384f4623b7b0acde0f0cfb460341
Shijuchi <shanedelatorre2412@gmail.com>
2024-06-22 01:29:29 +0800

    signup done and also modified the store folder for state management

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -12,27 +12,34 @@
       <div class="container">
         <div class="columns">
             <div class="column is-4 is-offset-4">
-                <form>
+                <form v-on:submit.prevent="submitForm">
                     <div class="field">
                         <label>Email:</label>
                         <div class="control">
-                            <input type="email" class="input">
+                            <input type="email" class="input" v-model="username">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Password:</label>
                         <div class="control">
-                            <input type="password" class="input">
+                            <input type="password" class="input" v-model="password">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Confirm Password:</label>
                         <div class="control">
-                            <input type="password" class="input">
+                            <input type="password" class="input" v-model="confirmPassword">
                         </div>
                     </div>
+
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" :key="error">
+                            {{ error }}
+                        </p>
+                    </div>
+
 
                     <div class="field">
                         <div class="control">
@@ -42,6 +49,7 @@
 
                 </form>
 
+                
                 <hr>
 
                 or <router-link :to="{name: 'login'}"> Click here </router-link> to log in!
@@ -54,7 +62,75 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    name: 'Signup',
+
+    data(){
+        return {
+            username: '',
+            password: '',
+            confirmPassword: '',
+            errors: []
+        }
+    },
+
+    methods: {
+        submitForm() {
+            console.log('Form submitted!');
+
+            this.errors = [];
+
+            if (this.username === ''){
+                this.errors.push('Please enter an email address');
+            }
+
+            if (this.password === ''){
+                this.errors.push('Please enter a password');
+            }
+
+            if (this.confirmPassword === ''){
+                this.errors.push('Please confirm your password');
+            }
+
+            if (this.confirmPassword !== this.password){
+                this.errors.push('Passwords do not match');
+            }
+
+            if(!this.errors.length){
+                const formData = {
+                    username: this.username,
+                    password: this.password
+                }
+
+                axios
+                .post('/api/v1/users/',formData)
+                .then(res =>{
+                    this.$router.push({name: 'login'})
+                })
+                .catch(err => {
+                    if(err.response){
+                        for(const property in err.response.data){
+                            this.errors.push(`${property}: ${err.response.data[property]}`)
+                        }
+
+                        console.log(JSON.stringify(err.response.data));
+
+                    }else if(err.message){
+                        this.errors.push('Something went wrong! Please try again.');
+
+                        console.log(JSON.stringify(err))
+                    }
+                })
+
+
+
+                console.log('Form submitted successfully!');
+            }
+        }
+    },
+
 
 }
 </script>
