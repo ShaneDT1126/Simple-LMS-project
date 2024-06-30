a94d9fb22f5909b0726147168b7f1f554656e474
Shijuchi <shanedelatorre2412@gmail.com>
2024-06-30 14:51:02 +0800

    refractored the quiz, move it to a component

---
+++ frontend/src/components/QuizView.vue
@@ -0,0 +1,72 @@
+<template>
+     <h3>{{ quiz.question }}</h3>
+
+    <div class="control">
+        <label class="radio">
+            <input type="radio" :value="quiz.op1" v-model="selectedAnswer"> {{ quiz.op1 }}
+        </label>
+    </div>
+
+    <div class="control">
+        <label class="radio">
+            <input type="radio" :value="quiz.op2" v-model="selectedAnswer"> {{ quiz.op2 }}
+        </label>
+    </div>
+
+    <div class="control">
+        <label class="radio">
+            <input type="radio" :value="quiz.op3" v-model="selectedAnswer"> {{ quiz.op3 }}
+        </label>
+    </div>
+
+    <div class="control mt-4">
+        <button class="button is-info" @click="submitQuiz" >Submit</button>
+    </div>
+
+    <template v-if="quizResult == 'Correct'">
+        <div class="notification is-success mt-4">Correct :D</div>
+    </template>
+
+    <template v-if="quizResult == 'Incorrect'">
+        <div class="notification is-danger mt-4">Incorrect :( Try again! </div>
+    </template>
+
+</template>
+
+<script>
+
+
+export default {
+    props: ['quiz'],
+
+    data(){
+        return {
+            selectedAnswer: '',
+            quizResult: null
+        }
+    },
+
+    methods:{
+        submitQuiz(){
+            this.quizResult = null;
+
+            if(this.selectedAnswer){
+
+                if(this.selectedAnswer === this.quiz.answer){
+                    this.quizResult = 'Correct';
+                }else{
+                    this.quizResult = 'Incorrect';
+                }
+
+            }else {
+                alert('Please select an answer');
+            }
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/CourseView.vue
+++ frontend/src/views/CourseView.vue
@@ -30,38 +30,8 @@
                                 <hr>
 
                                 <template v-if="activeLesson.lesson_type === 'quiz'">
-                                    <h3>{{ quiz.question }}</h3>
-
-                                    <div class="control">
-                                        <label class="radio">
-                                            <input type="radio" :value="quiz.op1" v-model="selectedAnswer"> {{ quiz.op1 }}
-                                        </label>
-                                    </div>
-
-                                    <div class="control">
-                                        <label class="radio">
-                                            <input type="radio" :value="quiz.op2" v-model="selectedAnswer"> {{ quiz.op2 }}
-                                        </label>
-                                    </div>
-
-                                    <div class="control">
-                                        <label class="radio">
-                                            <input type="radio" :value="quiz.op3" v-model="selectedAnswer"> {{ quiz.op3 }}
-                                        </label>
-                                    </div>
-
-                                    <div class="control mt-4">
-                                        <button class="button is-info" @click="submitQuiz" >Submit</button>
-                                    </div>
-
-                                    <template v-if="quizResult == 'Correct'">
-                                        <div class="notification is-success mt-4">Correct :D</div>
-                                    </template>
-
-                                    <template v-if="quizResult == 'Incorrect'">
-                                        <div class="notification is-danger mt-4">Incorrect :( Try again! </div>
-                                    </template>
-                                    
+                                   
+                                    <QuizViewVue :quiz="quiz" />
 
                                 </template>
 
@@ -97,13 +67,15 @@
 import axios from 'axios';
 import CourseCommentViewVue from '@/components/CourseCommentView.vue';
 import AddCommentViewVue from '@/components/AddCommentView.vue';
+import QuizViewVue from '@/components/QuizView.vue';
 
 export default {
     name: 'Course',
 
     components: {
         CourseCommentViewVue,
-        AddCommentViewVue
+        AddCommentViewVue,
+        QuizViewVue
     },
 
     data(){
@@ -113,10 +85,7 @@ export default {
             comments: [],
             errors: [],
             quiz: {},
-            selectedAnswer: '',
             activeLesson: null,
-            quizResult: null,
-    
         }
     },
 
@@ -154,21 +123,6 @@ export default {
             })
         },
 
-        submitQuiz(){
-            this.quizResult = null;
-
-            if(this.selectedAnswer){
-
-                if(this.selectedAnswer === this.quiz.answer){
-                    this.quizResult = 'Correct';
-                }else{
-                    this.quizResult = 'Incorrect';
-                }
-
-            }else {
-                alert('Please select an answer');
-            }
-        }
     },
 
     async mounted(){
