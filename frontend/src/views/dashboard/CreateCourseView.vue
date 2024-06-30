5fe2c6ae2f276e5df98c7366f3fe0700188e2475
Shijuchi <shanedelatorre2412@gmail.com>
2024-07-01 01:42:16 +0800

    improved create course: you can add lesson with it

--- frontend/src/views/dashboard/CreateCourseView.vue
+++ frontend/src/views/dashboard/CreateCourseView.vue
@@ -7,29 +7,59 @@
         </div>
 
         <section class="section">
-            <div class="field">
-                <label>Title</label>
-                <input type="text" v-model="course_form.title" class="input">
-            </div>
+            <div class="mb-6 px-6 py-4 has-background-white-ter">
+                <h2 class="subtitle">Meta Information</h2>
+                <div class="field">
+                    <label>Title</label>
+                    <input type="text" v-model="course_form.title" class="input">
+                </div>
                 
-            <div class="field">
-                <label>Short Description</label>
-                <textarea v-model="course_form.short_description" class="textarea"></textarea>
-            </div>
+                <div class="field">
+                    <label>Short Description</label>
+                    <textarea v-model="course_form.short_description" class="textarea"></textarea>
+                </div>
                 
-            <div class="field">
-                <label>Long Description</label>
-                <textarea v-model="course_form.long_description" class="textarea"></textarea>
+                <div class="field">
+                    <label>Long Description</label>
+                    <textarea v-model="course_form.long_description" class="textarea"></textarea>
+                </div>
+
+                <div class="field">
+                    <div class="select is-multiple">
+                        <select multiple size="10" v-model="course_form.categories" >
+                            <option v-for="category in categories" :key="category.id" :value="category.id">
+                                {{ category.title }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
             </div>
 
-            <div class="field">
-                <div class="select is-multiple">
-                    <select multiple size="10" v-model="course_form.categories" >
-                        <option v-for="category in categories" :key="category.id" :value="category.id">
-                            {{ category.title }}
-                        </option>
-                    </select>
+            <div class="mb-6 px-6 py-4 has-background-white-ter">
+                <h2 class="subtitle">Lessons</h2>
+
+                <div v-for="(lesson,index) in course_form.lessons" :key="index" class="mb-4">
+                    <h3 class="subtitle is-size-6">Lesson</h3>
+
+                    <div class="field">
+                        <label>Title</label>
+                        <input type="text" class="input" v-model="lesson.title" :name="`form.lesson[${index}][title]`" >
+                    </div>
+
+                    <div class="field">
+                        <label>Short Description</label>
+                        <textarea v-model="lesson.short_description" class="textarea" :name="`form.lesson[${index}][short_description]`" ></textarea>
+                    </div>
+                
+                    <div class="field">
+                        <label>Long Description</label>
+                        <textarea v-model="lesson.long_description" class="textarea" :name="`form.lesson[${index}][long_description]`" ></textarea>
+                    </div>
+
+                    <hr>
                 </div>
+
+                <button class="button is-primary is-outlined" @click="addLesson()" >Add Lesson</button>
             </div>
 
             <div class="field buttons">
@@ -56,6 +86,7 @@ export default {
                 long_description: '',
                 categories: [],
                 status: '',
+                lessons: [],
             },
             categories: [],
         }
@@ -82,7 +113,15 @@ export default {
                 .catch(error => {
                     console.log("Error",error);
                 })
-        }
+        },
+
+        addLesson(){
+            this.course_form.lessons.push({
+                title: '',
+                short_description: '',
+                long_description: '',
+            })
+        },
     },
 
     mounted(){
