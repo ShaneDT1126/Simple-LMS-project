c8ca1c72f78f7090d287a21624c106a2af08c564
Shijuchi <shanedelatorre2412@gmail.com>
2024-06-30 16:56:34 +0800

    added new serializer and new API for setting activity to started and done

--- backend/simple_lms/activity/views.py
+++ backend/simple_lms/activity/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Activity
 from course.serializers import CourseListSerializer
+from course.models import Course, Lessons
+
+from .serializers import ActivitySerializer
 
 
 @api_view(['GET'])
@@ -15,4 +18,33 @@ def get_active_courses(request):
             courses.append(activity.course)
 
     serializer = CourseListSerializer(courses, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def track_started(request, course_slug, lesson_slug):
+    course = Course.objects.get(slug=course_slug)
+    lesson = Lessons.objects.get(slug=lesson_slug)
+
+    if Activity.objects.filter(created_by=request.user, course=course, lesson=lesson).count() == 0:
+        Activity.objects.create(course=course, lesson=lesson, created_by=request.user)
+
+    activity = Activity.objects.get(created_by=request.user, course=course, lesson=lesson)
+
+    serializer = ActivitySerializer(activity, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def mark_as_done(request, course_slug, lesson_slug):
+    course = Course.objects.get(slug=course_slug)
+    lesson = Lessons.objects.get(slug=lesson_slug)
+
+    activity = Activity.objects.get(created_by=request.user, course=course, lesson=lesson)
+    activity.status = Activity.DONE
+    activity.save()
+
+    serializer = ActivitySerializer(activity)
+
     return Response(serializer.data)
