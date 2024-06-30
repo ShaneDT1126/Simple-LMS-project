6f73e41594dbbdfab81f7c4a80288776bf820f0d
jogzfon <jogzfon@gmail.com>
2024-06-30 23:36:29 +0800

    AssemblyError

---
+++ frontend/src/carp/AssemblyError.js
@@ -0,0 +1,22 @@
+class AssemblyError {
+    constructor(lineNumber = 0, str = "") {
+        this.lineNumber = lineNumber;
+        this.str = str;
+    }
+
+    getLineNumber() {
+        return this.lineNumber;
+    }
+
+    getString() {
+        return this.str;
+    }
+
+    setLineNumber(lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    setString(str) {
+        this.str = str;
+    }
+}
